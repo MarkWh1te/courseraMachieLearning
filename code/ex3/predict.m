173291dd9b90a33b3eb975f5ed6f6abab9bfb567
MarkWh1te <iamwh1temark@outlook.com>
2017-03-02 21:37:14 +0800

    finished week5 and I feel so happy today

--- code/ex3/predict.m
+++ code/ex3/predict.m
@@ -1,5 +1,4 @@
-function p = predict(Theta1, Theta2, X)
-%PREDICT Predict the label of an input given a trained neural network
+function p = predict(Theta1, Theta2, X) %PREDICT Predict the label of an input given a trained neural network
 %   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
 %   trained weights of a neural network (Theta1, Theta2)
 
@@ -7,12 +6,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
